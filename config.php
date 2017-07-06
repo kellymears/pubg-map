8c7ddeaf6a0e41ae43d52c270cb15296e5efdb0b
Kelly Mears <code@kellymears.me>
2017-07-06 16:20:21 -0700

    more server code. broke mysqli into its own class.

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$db_servername = 'localhost';
+$db_username = 'username';
+$db_password = 'password';
+
+?>
--- server.php
+++ server.php
@@ -1,17 +1,68 @@
 <?php
 
+require "config.php";
+
+class map_db {
+
+  var $servername;
+  var $username;
+  var $password;
+  var $conn;
+
+  function __construct($db_servername,$db_username,$db_password) {
+    $this->servername = $db_servername;
+    $this->username = $db_username;
+    $this->password = $db_password;
+    $this->connect_to_db();
+  }
+
+  function connect_to_db() {
+    $this->conn = new_mysqli($this->servername,$this->username,$this->password);
+    if ($this->conn->connect_error) {
+      die("Connection failed: " . $this->conn->connect_error);
+    }
+    echo "Database connection successful";
+  }
+
+
+
+}
+
 class map {
 
- var $var;
+ var $db;
 
- function __construct($var="var") {
-     $this->var = $var;
+ function __construct() {
+     $this->db = new map_db($db_servername,$db_username,$db_password)
  }
 
- function is_var() {
-     return $this->var;
+ function create_poi($name,$type,$lat,$long) {
+   $sql = "INSERT INTO map_pois (name, type, lat, long)
+          VALUES ($name, $type, $lat, $long)";
+
+   if($this->db->conn->query($sql) === TRUE) {
+     echo "New record created successfully";
+   } else {
+     echo "Error: " . $sql . "<br>" . $this->db->conn->error;
+   }
  }
 
-} 
+ function read_poi($id) {
+
+ }
+
+ function update_poi($id,$name,$type,$lat,$long) {
+
+ }
+
+ function destroy_poi($id) {
+
+ }
+
+ function retrieve_poi() {
+
+ }
+
+}
 
 ?>
