31c99b6353b8d33939cf34c4fa2a5ce534d082fa
rawrimbumblebee <rawrimbumblebee@gmail.com>
2017-07-11 14:01:51 -0700

    Made it so that users do not have to know a map name into the URL bar
     * This will pull up map "default" with no poi's on the map
     * Made some style changes to length of some lines on accident in this commit

--- index.php
+++ index.php
@@ -18,7 +18,9 @@
 
   <ul class="vertical medium-horizontal menu" style="background:black;">
   <li>
-  <li><h5 style="position:relative;top:.2em; padding-left:.5em; color:white;"><i class="fi-map"></i> Mapster</h5></li>
+  <li><h5 style="position:relative;top:.2em; padding-left:.5em; color:white;">
+    <i class="fi-map"></i> Mapster</h5>
+  </li>
   <li><a href="#0" style="color:white;"><i class="fi-list"></i> <span>One</span></a></li>
   <li><a href="#0" style="color:white;"><i class="fi-list"></i> <span>Two</span></a></li>
   <li><a href="#0" style="color:white;"><i class="fi-list"></i> <span>Three</span></a></li>
@@ -71,8 +73,14 @@
         return this._div;
     };
 
+    var whichMap = "<?php if(isset($_GET['map'])) { echo $_GET['map']; } ?>";
+    if (!whichMap) {
+      whichMap = "default";
+    }
+
     info.update = function (props) {
-        this._div.innerHTML = '<h4>Player Unknown\'s Battlegrounds</h4> <b>Viewing map:</b> <?php echo $_GET['map']; ?>';
+        this._div.innerHTML = '<h4>Player Unknown\'s Battlegrounds</h4> \
+                                <b>Viewing map:</b> ' + whichMap;
     };
 
     info.addTo(map);
@@ -80,16 +88,20 @@
     /* display markers */
 
     $.ajax({
-      method: "GET",
+      method: "POST",
       url: "requests.php",
-      data: { request: "readAll", map: "<?php echo $_GET['map']; ?>" }
+      data: { request: "readAll", map: whichMap }
     })
       .done(function( data ) {
-        var json_data = $.parseJSON(data);
-        $.each(json_data, function(key,value) {
-          document["marker" + value.id] = L.marker([parseFloat(value.lat), parseFloat(value.long)]).addTo(map);
-          document["marker" + value.id].bindPopup('<b>' + value.name + '</b>').openPopup();
-        });
+        if (data) {
+          var json_data = JSON.parse(data);
+          $.each(json_data, function(key,value) {
+            document["marker" + value.id] =
+              L.marker([parseFloat(value.lat),
+                        parseFloat(value.long)]).addTo(map);
+            document["marker" + value.id].bindPopup('<b>' + value.name + '</b>').openPopup();
+          });
+        }
       });
 
       /* on click interactivity */
@@ -99,7 +111,10 @@
       function onMapClick(e) {
           popup
               .setLatLng(e.latlng)
-              .setContent("You clicked the map at " + e.latlng.toString() + "<br><a href='requests.php?request=create&name=Test&type=1&lat="+ e.latlng.lat +"&long="+ e.latlng.lng +"&map=<?php echo $_GET['map']; ?>'>Test</a>")
+              .setContent("You clicked the map at " + e.latlng.toString() +
+                "<br><a href='requests.php?request=create&name=Test&type=1&lat=" +
+                e.latlng.lat +"&long="+ e.latlng.lng +
+                "&map=" + whichMap + "'>Test</a>")
               .openOn(map);
       }
 
