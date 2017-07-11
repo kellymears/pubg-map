aa04477a6c499813c52985ccc6df3feac0c55538
rawrimbumblebee <rawrimbumblebee@gmail.com>
2017-07-11 14:00:00 -0700

    Changed the GETS to POSTS

--- requests.php
+++ requests.php
@@ -1,28 +1,28 @@
 <?php
 
   require "server.php";
-  $squad_pubg = new map($db_servername,$db_username,$db_password,$db_name,$_GET["map"]);
+  $squad_pubg = new map($db_servername,$db_username,$db_password,$db_name,$_POST["map"]);
 
-  if($_GET['map']) {
+  if($_POST['map']) {
 
-    if($_GET['request']=="create") {
-      $result = $squad_pubg->create_poi($_GET['name'],$_GET['type'],$_GET['lat'],$_GET['long'],$_GET['map']);
+    if($_POST['request']=="create") {
+      $result = $squad_pubg->create_poi($_POST['name'],$_POST['type'],$_POST['lat'],$_POST['long'],$_POST['map']);
       echo $result;
     }
 
-    /* if($_GET['request']=="update") {
-      $result = $squad_pubg->update_poi($_GET['id'],$_GET['name'],$_GET['type'],$_GET['lat'],$_GET['long'],$_GET['map']);
+    /* if($_POST['request']=="update") {
+      $result = $squad_pubg->update_poi($_POST['id'],$_POST['name'],$_POST['type'],$_POST['lat'],$_POST['long'],$_POST['map']);
     }
 
-    if($_GET['request']=="delete") {
-      $result = $squad_pubg->destroy_poi($_GET['id']);
+    if($_POST['request']=="delete") {
+      $result = $squad_pubg->destroy_poi($_POST['id']);
     }
 
-    if($_GET['request']=="read") {
-      $result = $squad_pubg->read_poi($_GET['id']);
+    if($_POST['request']=="read") {
+      $result = $squad_pubg->read_poi($_POST['id']);
     } */
 
-    if($_GET['request']=="readAll") {
+    if($_POST['request']=="readAll") {
       $result = $squad_pubg->display_pois();
     }
 
