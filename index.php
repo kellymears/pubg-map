c2820f41f5e5064cc5d5a543bde707d38d9dda1a
Kelly Mears <code@kellymears.me>
2017-07-12 08:49:29 -0700

    added js back to footer area

--- index.php
+++ index.php
@@ -7,17 +7,8 @@
   <title>Mapster</title>
   <link rel="icon" type="image/png" href="images/pubg.png"/>
 
-  <!-- jQuery -->
-  <script type="text/javascript" src="dist/jquery-3.2.1.min.js"> </script>
-
   <!-- Bootstrap -->
   <link href="dist/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-
-  <!-- Bootstrap select plugin -->
-  <script type="text/javascript"
-          src="dist/bootstrap-select-1.12.3/js/bootstrap-select.min.js">
-  </script>
-
 
   <!-- Foundation Icons -->
   <link rel="stylesheet" href="dist/foundation/icons/foundation-icons.css">
@@ -47,11 +38,16 @@
   <!-- map -->
   <div id="map"></div>
 
-  <!-- jquery -->
-  <script src="dist/foundation/js/vendor/jquery.js"></script>
+  <!-- jQuery -->
+  <script src="dist/jquery-3.2.1.min.js"> </script>
 
   <!-- bootstrap js -->
   <script src="dist/bootstrap/js/bootstrap.js"></script>
+
+  <!-- Bootstrap select plugin -->
+  <script type="text/javascript"
+          src="dist/bootstrap-select-1.12.3/js/bootstrap-select.min.js">
+  </script>
 
   <!-- leaflet js -->
   <script src="dist/leaflet/leaflet.js"></script>
